f117c271c4ef109ea027c229eb9bd4a3fdc386bf
Boykalanders <boykalanders@gmail.com>
2024-07-10 09:49:13 -0400

    feat: create appwrite and create db and 3 collections

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -6,37 +6,53 @@ import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import CustomFormField from "./CustomFormField";
+import SubmitButton from "./SubmitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
 
 export enum FormFieldType {
   INPUT = "input",
   TEXTAREA = "textarea",
   PHONE_INPUT = "phoneInput",
-  CHECKBOX = 'checkbox',
+  CHECKBOX = "checkbox",
   DATE_PICKER = "datePicker",
   SELECT = "select",
-  SKELTON = "skeleton"
+  SKELTON = "skeleton",
 }
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
-
 const PatientForm = () => {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
       username: "",
+      email: "",
+      phone: "",
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit({
+    username,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+    try {
+      const userData = {
+        username,
+        email,
+        phone,
+      };
+
+      const user = await createUser(userData);
+
+      if(user) router.push(`/patients/${user.$id}/register`)
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
@@ -49,13 +65,13 @@ const PatientForm = () => {
         <CustomFormField
           fieldType={FormFieldType.INPUT}
           control={form.control}
-          name="name"
+          name="username"
           label="Full Name"
           placeholder="Patrick"
           iconSrc="/assets/icons/user.svg"
           iconAlt="user"
         />
-        
+
         <CustomFormField
           fieldType={FormFieldType.INPUT}
           control={form.control}
@@ -73,7 +89,7 @@ const PatientForm = () => {
           label="Phone number"
           placeholder="(555) 123-4567"
         />
-        <Button type="submit">Submit</Button>
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
   );
