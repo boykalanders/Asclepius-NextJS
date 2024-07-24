a0654b66e65f674bae7d9dea3e14c34f7702be73
Boykalanders <boykalanders@gmail.com>
2024-07-24 05:22:23 -0400

    chore: add logo to the page

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,9 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
+import logo from "@/public/assets/icons/logo-full.svg";
 
 const page = () => {
   return (
-    <div>
-      Register
+    <div className='flex h-screen max-h-screen'>
+      <section className='remove-scollbar container'>
+        <div className='sub-container max-w-[860px] flex-1 flex-col py-10'>
+          <Image 
+            src={logo}
+            height={1000}
+            width={1000}
+            alt="asclepius"
+            className="mb-12 h-10 w-fit"
+          />
+          
+        </div>
+      </section>
     </div>
   )
 }
